dca3af6530ef1fd0bf7eabac5fbc18c18e349d5d
felipeeco <contacto@felipemoreno.co>
2024-01-09 08:26:23 -0500

    inputs and outputs

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -8,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './counter.component.css'
 })
 export class CounterComponent {
+  @Input({required: true}) duration = 0;
+  @Input({required: true}) message = '';
 
+  constructor() {
+    console.log(this.duration + this.message);
+  }
+
+  ngOnChanges() {
+    // before render and during
+  }
 }
