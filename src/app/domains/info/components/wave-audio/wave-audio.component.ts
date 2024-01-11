78117ee8d45498d7cf4a5439304abeee50c66476
felipeeco <contacto@felipemoreno.co>
2024-01-10 22:29:45 -0500

    do wave-audio component

---
+++ src/app/domains/info/components/wave-audio/wave-audio.component.ts
@@ -0,0 +1,37 @@
+import { Component, ElementRef, Input, ViewChild, Inject, PLATFORM_ID, signal } from '@angular/core';
+import { CommonModule, DOCUMENT, isPlatformBrowser } from '@angular/common';
+import WaveSurfer from 'wavesurfer.js';
+
+@Component({
+  selector: 'app-wave-audio',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './wave-audio.component.html',
+  styleUrl: './wave-audio.component.css'
+})
+
+export class WaveAudioComponent {
+
+  constructor(@Inject(DOCUMENT) private document: Document, @Inject(PLATFORM_ID) private platformId: Object) { }
+
+  @Input({required: true}) audioUrl!: string;
+  @ViewChild('wave') container!: ElementRef;
+  private ws!: WaveSurfer;
+  isPlaying = signal(false);
+  
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId) && this.document) {
+      this.ws = WaveSurfer.create({
+        url: this.audioUrl,
+        container: this.container.nativeElement
+      });
+
+      this.ws?.on('play', () => this.isPlaying.set(true));
+      this.ws?.on('pause', () => this.isPlaying.set(false));
+    }
+  }
+
+  playPause() {
+    this.ws.playPause();
+  }
+}
