8f2f4f078707df0649a85253fa312ca7777cfcd2
Luis Felipe Moreno Rondon <luisfelipe.moreno.contractor@bbva.com>
2024-01-14 20:55:41 -0500

    do header

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,17 +1,46 @@
 import { Component, signal } from '@angular/core';
 import { ProductComponent } from '../../components/product/product.component';
 import { Product } from '../../../shared/models/product.model';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [ProductComponent],
+  imports: [ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
 
   products = signal<Product[]>([
+    {
+      id: Date.now(),
+      title: 'Product one',
+      price: 100,
+      imageUrl: 'https://picsum.photos/640/640?r=23',
+      creationAt: new Date().toISOString()
+    },
+    {
+      id: Date.now(),
+      title: 'Product two',
+      price: 80,
+      imageUrl: 'https://picsum.photos/640/640?r=12',
+      creationAt: new Date().toISOString()
+    },
+    {
+      id: Date.now(),
+      title: 'Product one',
+      price: 100,
+      imageUrl: 'https://picsum.photos/640/640?r=23',
+      creationAt: new Date().toISOString()
+    },
+    {
+      id: Date.now(),
+      title: 'Product two',
+      price: 80,
+      imageUrl: 'https://picsum.photos/640/640?r=12',
+      creationAt: new Date().toISOString()
+    },
     {
       id: Date.now(),
       title: 'Product one',
