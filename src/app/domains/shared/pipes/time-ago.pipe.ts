5ba5e84e7754a9399fe55d9f80046e0f60295080
Luis Felipe Moreno Rondon <luisfelipe.moreno.contractor@bbva.com>
2024-01-24 18:31:54 -0500

    Do custom directive

--- src/app/domains/shared/pipes/time-ago.pipe.ts
+++ src/app/domains/shared/pipes/time-ago.pipe.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { formatDistance } from 'date-fns';
 
 @Pipe({
   name: 'timeAgo',
@@ -7,23 +8,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TimeAgoPipe implements PipeTransform {
 
   transform(dateString: string): string {
-    const now: Date = new Date();
-    const timeDifferenceMilliseconds: number = now.getTime() - Date.parse(dateString);
-    const secondsElapsed: number = Math.round(timeDifferenceMilliseconds / 1000);
-    const minutesElapsed: number = Math.round(secondsElapsed / 60);
-    const hoursElapsed: number = Math.round(minutesElapsed / 60);
-    const daysElapsed: number = Math.round(hoursElapsed / 24);
-    const monthsElapsed: number = Math.round(daysElapsed / 30);
-
-    if (minutesElapsed < 60) {
-        return `${minutesElapsed} minutes ago`;
-    } else if (hoursElapsed < 24) {
-        return `${hoursElapsed} hours ago`;
-    } else if (daysElapsed < 30) {
-        return `${daysElapsed} days ago`;
-    } else {
-        return `${monthsElapsed} months ago`;
-    }
+    const date = new Date(dateString);
+    const today = new Date();
+    return formatDistance(today, date);
   }
 
 }
