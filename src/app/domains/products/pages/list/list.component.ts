23d4fe78691fec8521f9bae52c17652ce773e616
Luis Felipe Moreno Rondon <luisfelipe.moreno.contractor@bbva.com>
2024-01-24 20:19:02 -0500

    do 404 page

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,14 +1,14 @@
 import { Component, signal, inject, OnInit } from '@angular/core';
 import { ProductComponent } from '@products/components/product/product.component';
 import { Product } from '@shared/models/product.model';
-import { HeaderComponent } from '@shared/components/header/header.component';
+
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [ProductComponent, HeaderComponent],
+  imports: [ProductComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
